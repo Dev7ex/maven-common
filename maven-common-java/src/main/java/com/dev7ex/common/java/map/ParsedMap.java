6d8d9bc5c412b0828c47e6e57227175a83f5326e
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2022-05-04 23:48:49 +0200

    adding constructor for initialCapacity

--- maven-common-java/src/main/java/com/dev7ex/common/java/map/ParsedMap.java
+++ maven-common-java/src/main/java/com/dev7ex/common/java/map/ParsedMap.java
@@ -4,28 +4,51 @@ import java.util.HashMap;
 import java.util.UUID;
 
 /**
- *
  * @author Dev7ex
  * @since 02.11.2020
- *
  */
-
 public final class ParsedMap<K, V> extends HashMap<K, V> {
+
+    public ParsedMap() {}
+
+    public ParsedMap(final int initialCapacity) {
+        super(initialCapacity);
+    }
 
     public <V> V getValue(final String key) {
         return (V) this.get(key);
+    }
+
+    public <V> V getValue(final String key, final V defaultValue) {
+        return (this.get(key) == null ? defaultValue : (V) this.get(key));
     }
 
     public final String getString(final K key) {
         return super.get(key).toString();
     }
 
+    public final String getString(final K key, final String defaultValue) {
+        return (super.get(key).toString() == null ? defaultValue : super.get(key).toString());
+    }
+
     public final byte getByte(final K key) {
         return Byte.parseByte(this.getString(key));
     }
 
+    public final byte getByte(final K key, final byte defaultValue) {
+        return (this.getString(key) == null ? defaultValue : Byte.parseByte(this.getString(key)));
+    }
+
     public final short getShort(final K key) {
         return Short.parseShort(this.getString(key));
+    }
+
+    public final short getShort(final K key, final short defaultValue) {
+        return (this.getString(key) == null ? defaultValue : Short.parseShort(this.getString(key)));
+    }
+
+    public final int getInteger(final K key,  final int defaultValue) {
+        return (this.getString(key) == null ? defaultValue : Integer.parseInt(this.getString(key)));
     }
 
     public final int getInteger(final K key) {
@@ -36,16 +59,32 @@ public final class ParsedMap<K, V> extends HashMap<K, V> {
         return Long.parseLong(this.getString(key));
     }
 
+    public final long getLong(final K key, final long defaultValue) {
+        return (this.getString(key) == null ? defaultValue : Long.parseLong(this.getString(key)));
+    }
+
     public final float getFloat(final K key) {
         return Float.parseFloat(this.getString(key));
+    }
+
+    public final float getFloat(final K key, final float defaultValue) {
+        return (this.getString(key) == null ? defaultValue : Float.parseFloat(this.getString(key)));
     }
 
     public final double getDouble(final K key) {
         return Double.parseDouble(this.getString(key));
     }
 
+    public final double getDouble(final K key, final double defaultValue) {
+        return (this.getString(key) == null ? defaultValue : Double.parseDouble(this.getString(key)));
+    }
+
     public final boolean getBoolean(final K key) {
         return Boolean.parseBoolean(this.getString(key));
+    }
+
+    public final boolean getBoolean(final K key, final boolean defaultValue) {
+        return (this.getString(key) == null ? defaultValue : Boolean.parseBoolean(this.getString(key)));
     }
 
     public final UUID getUUID(final K key) {
